be44d9069e0fe46b5240cac4e5d195f82d102dbb
rootnure <nur.diu.2791@gmail.com>
2023-10-27 02:17:23 +0600

    Intro to TanStack Query (React Query)

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,21 @@ import './index.css'
 import router from './route/Router.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-      </HelmetProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
